70b189522bd24853fb4d9c36a7c6e81d81b8a952
guialvesped <alvespedrosog@gmail.com>
2025-05-21 16:07:34 -0300

    Add dialog for target

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import TargetDialog from "@/components/TargetDialog";
+import { CircleCheckBig } from "lucide-react";
+
+export default function DashboardPage() {
+  return (
+    <main className="flex flex-col items-center min-h-screen p-4 bg-black">
+      <div className="flex flex-row items-center justify-center gap-3">
+        <CircleCheckBig className="text-white w-28 h-38" />
+        <h1 className="text-white text-[36px]">TaskFlow</h1>
+      </div>
+      <TargetDialog />
+    </main>
+  );
+}
