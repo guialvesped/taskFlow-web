69c826d67e7a4b7ae8a698e79e4a80eadc3bab29
guialvesped <alvespedrosog@gmail.com>
2025-05-23 11:59:01 -0300

    Add logout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,13 +2,21 @@
 
 import { TodoCard } from "@/components/TodoCard";
 import TodoDialog from "@/components/TodoDialog";
-import { CircleCheckBig } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { CircleCheckBig, LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { getTodos } from "./action";
 
 export default function DashboardPage() {
   const [todos, setTodos] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    router.push("/login");
+  };
 
   useEffect(() => {
     async function fetchTodos() {
@@ -33,35 +41,42 @@ export default function DashboardPage() {
   }, []);
 
   return (
-    <main className="flex flex-col items-center min-h-screen p-4 bg-black">
-      <div className="flex flex-row items-center justify-center gap-3 mb-6">
-        <CircleCheckBig className="text-white w-28 h-38" />
-        <h1 className="text-white text-[36px]">TaskFlow</h1>
-      </div>
-
-      <TodoDialog setTodos={setTodos} />
-
-      {loading ? (
-        <p className="text-white mt-8">Carregando...</p>
-      ) : (
-        <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-6xl">
-          {todos.map((todo) => (
-            <TodoCard
-              key={todo.id}
-              todo={{
-                id: todo.id,
-                title: todo.title,
-                description: todo.description,
-                todo_status: todo.todo_status,
-                priority: todo.priority,
-                dificulty: todo.dificulty,
-                due_date: todo.due_date,
-              }}
-              setTodos={setTodos}
-            />
-          ))}
+    <>
+      <header className="bg-black items-center">
+        <Button className="m-8" onClick={logout}>
+          <LogOut />
+        </Button>
+      </header>
+      <main className="flex flex-col items-center min-h-screen p-4 bg-black">
+        <div className="flex flex-row items-center justify-center gap-3 mb-6">
+          <CircleCheckBig className="text-white w-28 h-38" />
+          <h1 className="text-white text-[36px]">TaskFlow</h1>
         </div>
-      )}
-    </main>
+
+        <TodoDialog setTodos={setTodos} />
+
+        {loading ? (
+          <p className="text-white mt-8">Carregando...</p>
+        ) : (
+          <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-6xl">
+            {todos.map((todo) => (
+              <TodoCard
+                key={todo.id}
+                todo={{
+                  id: todo.id,
+                  title: todo.title,
+                  description: todo.description,
+                  todo_status: todo.todo_status,
+                  priority: todo.priority,
+                  dificulty: todo.dificulty,
+                  due_date: todo.due_date,
+                }}
+                setTodos={setTodos}
+              />
+            ))}
+          </div>
+        )}
+      </main>
+    </>
   );
 }
